4813a9af37ce5bffc38967bae1d75d459ec38b33
Chatchawan Koedsawas <121638748+petechatchawan@users.noreply.github.com>
2026-07-22 13:12:44 +0700

    feat: expose modern parser from package root

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-export { UAInfo } from './main/ua-info';
-export type {
-    ApplicationContext,
-    ApplicationInfo,
-    ApplicationName,
-    BrowserInfo,
-    BrowserName,
-    DeviceInfo,
-    OSInfo,
-    OSName,
-    UserAgentInfo,
-} from './types';
+export * from './v2';
